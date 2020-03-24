9ab9f1bb94fa8d0cc8db5a37440194e68a926909
gellati <omar.el-begawy@hiq.fi>
2020-03-24 19:08:52 +0200

    example with fetch request

---
+++ redux_thunk_middleware_asynchronous_actions_PScY3vwtXgo/README.md
@@ -0,0 +1,15 @@
+# Redux-thunk in get request
+
+The project exemplifies the use of `redux-thunk` for a fetch request. Blank webpage, output can be seen in the developer console.
+
+Install the dependencies:
+
+    npm i
+
+Usage:
+
+    npm run start
+
+Now open a browser in `http://localhost:8080/`.
+
+Based on this [tutorial](https://www.youtube.com/watch?v=PScY3vwtXgo)
---
+++ redux_thunk_middleware_asynchronous_actions_PScY3vwtXgo/src/index.js
@@ -0,0 +1,60 @@
+import { createStore, applyMiddleware } from "redux"
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+const initialState = {
+	loading: false,
+	loaded: false,
+	posts: [],
+	error: null
+}
+
+const reducer = ( state = initialState, action ) => {
+	switch ( action.type ) {
+		case "LOADING": {
+			return { ...state, loading: true }
+		}
+		case "LOADED": {
+			return { ...state, loading: false, loaded: true, posts: action.payload }
+		}
+		case "ERROR": {
+			return { ...state, loading: false, error: action.payload }
+		}
+	}
+
+	return  state
+}
+
+
+const middleware = applyMiddleware( thunk, logger )
+
+/**
+ * Create store using the reducer function added above
+ * and pass an initial state.
+ */
+const store = createStore( reducer, middleware )
+
+/**
+ * Action creators
+ * Lets dispatch an action.
+ * Multiple synchronous actions
+ * Instead of passing an object containing type and payload as store.dispatch parameter, we can pass a multiple dispatch functions
+ */
+store.dispatch( ( dispatch ) => {
+	// First Action Dispatch
+	dispatch( {type: 'LOADING'} )
+
+	fetch('https://jsonplaceholder.typicode.com/posts')
+		.then(response => response.json())
+		.then(jsonData => {
+
+			// Second Action Dispatch
+			dispatch( { type: 'LOADED', payload: jsonData } )
+		})
+		.catch( err => {
+
+			// Third Action Dispatch
+			dispatch( { type: 'ERROR', payload: err } )
+		} )
+
+} )
